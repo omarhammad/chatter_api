893d82d7b4e7979b449a020c33c1ceed7048fc89
Omarhammad_97 <omar.hammad@student.kdg.be>
2024-06-20 22:13:09 +0200

    Project structure

--- src/main/java/com/example/chatter/ChatterApplication.java
+++ src/main/java/com/example/chatter/ChatterApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.chatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ChatterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(com.example.chatter.ChatterApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/chatter/ChatterApplicationTests.java
+++ src/test/java/com/example/chatter/ChatterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.chatter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ChatterApplicationTests {
 
 	@Test
 	void contextLoads() {
